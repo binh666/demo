c398add5281ae5fa44cf2b856cbcd93f24ee6c3f
Binh 666 <phambinh19323@gmail.com>
2025-11-30 00:12:37 +0700

    Initial commit

---
+++ src/main/java/com/example/taingheshop/TaingheshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.taingheshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaingheshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaingheshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taingheshop/TaingheshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taingheshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaingheshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
