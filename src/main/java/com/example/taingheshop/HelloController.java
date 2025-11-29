2965157f6ef9bd1d5dffc86f0328c0f75742ae1a
Binh 666 <phambinh19323@gmail.com>
2025-11-30 00:19:39 +0700

    helo

--- src/main/java/com/example/taingheshop/HelloController.java
+++ src/main/java/com/example/taingheshop/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boot!";
+        return "Hello World from  Boot!";
     }
 
 }
